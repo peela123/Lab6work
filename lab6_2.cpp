cc5381698b36bebe30e716111fee3b4d685b8929
lab314-74 <lab314-74@labeng.local>
2020-12-25 18:00:15 +0700

    งานlab6

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,41 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    int id;
+    string moviename;
+    string day;
+    string phrase;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n";
+    cout << "Me: ";
+    
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << "\n";
+    
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << "\n";
+    cout  << name << ": ";
+    cin >> id;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR " << (id/10000000)-12 << ". I have a free movie ticket for you." << "\n";
+    cout << "Fahsai: Let's go to the cinema together!!!" << "\n";
+    cout << "Fahsai: What movie do you want to watch?" << "\n";
+    
+    cout << name << ": ";
+    getline(cin,moviename);
+    cout << "Fahsai: So....which day are you free to go with me?" << "\n";
+    
+    cout << name << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day << "...." << "that is OK!!! I'm looking forward to watching " << moviename <<  " with you." << "\n";
+    
+    cout << name << ": ";
+    getline(cin,phrase);
+    cout << "Fahsai: 555+ see you " << day << ". " << "Bye Bye \\(^ ^)/";
+
+
+    
+
+}
--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x == 65){
+		return x = 'Z';
+	}
+	if(x >= 65 && x <= 90){
+        x = x - 1;
+	}
+	else{
+		return '0';
+	}
+	return x;
 }
 
 int main(){
